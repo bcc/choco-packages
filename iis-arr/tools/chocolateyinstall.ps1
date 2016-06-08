1c0e2ef76505a6f2f6ce2f4a5bc30e54a40397de
Eric Swann <eric.swann@q2ebanking.com>
2016-06-08 15:18:24 -0500

    Updated temp path...perhaps default choco temp path changed but no longer need to manually add 'chocolatey' to it

--- iis-arr/tools/chocolateyinstall.ps1
+++ iis-arr/tools/chocolateyinstall.ps1
@@ -14,7 +14,7 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName)\$($packageName).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'Microsoft Application Request Routing 3.0'
